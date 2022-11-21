09847fb76db583d3e045ac1f50c0ff9bdb1b7644
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-21 18:11:35 -0500

    add comments

--- wordsearch.js
+++ wordsearch.js
@@ -1,14 +1,14 @@
 //Written by Anne-Marie Champoux and Aarij Anwer
 
+//searchs 2D array `letters` individually to see if `word` exists, returns true if it does, false otherwise
 const searchHorizontal = function(letters, word) {
   const horizontalJoin = letters.map(ls => ls.join(''));
   for (let l of horizontalJoin) {
-    //console.log(l);
     if (l.includes(word)) return true;
   }
   return false;
 };
-  
+
 const transpose = function(matrix) {
   let newArray = [];
   for (let i = 0; i < matrix.length; i++) {
@@ -22,11 +22,12 @@ const transpose = function(matrix) {
   return newArray;
 };
   
-// first do a horizontal search (could be a function that is called)
+//wordSearch function takes a 2D array `letters` and looks for `word` in it vertically and horizontally, returns true if word is found, false otherwise
 const wordSearch = (letters, word) => {
-  word = word.toUpperCase();
-  let answer = searchHorizontal(letters, word);
+  word = word.toUpperCase(); //ensure lowercase is not a problem
+  let answer = searchHorizontal(letters, word); //search horizontally
   if (!answer) {
+    //if not found horizontally, transpose the matrix and search horizontally again (vertically in effect)
     let vertical = transpose(letters);
     answer = searchHorizontal(vertical, word);
   }
