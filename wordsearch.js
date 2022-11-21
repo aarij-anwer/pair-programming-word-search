ed334297b07ec899253f6b038ebfa0e6b04c2e1c
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-21 18:03:05 -0500

    code written with partner Anne-Marie Champoux

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => {
+//Written by Anne-Marie Champoux and Aarij Anwer
+
+const searchHorizontal = function(letters, word) {
   const horizontalJoin = letters.map(ls => ls.join(''));
-  for (l of horizontalJoin) {
+  for (let l of horizontalJoin) {
+    console.log(l);
     if (l.includes(word)) return true;
   }
+  return false;
 };
-
-module.exports = wordSearch;
+  
+const transpose = function(matrix) {
+  let newArray = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!Array.isArray(newArray[j])) {
+        newArray[j] = [];
+      }
+      newArray[j][i] = matrix[i][j];
+    }
+  }
+  return newArray;
+};
+  
+// first do a horizontal search (could be a function that is called)
+const wordSearch = (letters, word) => {
+  word = word.toUpperCase();
+  let answer = searchHorizontal(letters, word);
+  if (!answer) {
+    let vertical = transpose(letters);
+    answer = searchHorizontal(vertical, word);
+  }
+  return answer;
+};
+  
+  
+  
